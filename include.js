6436f3c93622f4264b88eb956166a56a4b54ff68
hiroyuki-n <ubyave@gmail.com>
2020-07-27 06:46:33 +0900

    include

--- include.js
+++
@@ -1,9 +0,0 @@
-function header() {
-    $.ajax({
-        url: "include/header.html", // 読み込むHTMLファイル
-        cache: false,
-        success: function (html) {
-            document.write(html);
-        }
-    });
-}
